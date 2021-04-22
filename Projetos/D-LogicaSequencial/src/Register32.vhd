974418deafed0dcfb3ccfe5adde2835268a1ee23
borg <pedrosz@al.insper.edu.br>
2021-04-22 18:36:28 -0300

    Register32 funcionando e testado

--- Projetos/D-LogicaSequencial/src/Register32.vhd
+++ Projetos/D-LogicaSequencial/src/Register32.vhd
@@ -24,8 +24,23 @@ architecture arch of Register32 is
 			output: out STD_LOGIC_VECTOR(15 downto 0)
       );
 	end component;
+	signal entrada: STD_LOGIC_VECTOR(31 downto 0);
+	signal saida: STD_LOGIC_VECTOR(31 downto 0);
+	
 
 begin
+	entrada <= input;
+	
+	
+	register1: Register16
+	port map(
+		clock, entrada(31 downto 16), load, saida(31 downto 16)); 
+	
 
-
+	register2: Register16
+	port map(
+		clock, entrada(15 downto 0), load, saida(15 downto 0));
+		
+	output <= saida;
+		
 end architecture;
